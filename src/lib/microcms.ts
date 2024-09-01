541acbd208c41193c6b05b0ff90f8779ae46dce2
naoki.karishuku <naokifoot620@yahoo.co.jp>
2024-09-01 14:32:45 +0900

    feat: setting and test

---
+++ src/lib/microcms.ts
@@ -0,0 +1,26 @@
+//SDK利用準備
+import type { MicroCMSQueries } from "microcms-js-sdk";
+import { createClient } from "microcms-js-sdk";
+import type { Image } from "../types/image";
+
+const client = createClient({
+  serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: import.meta.env.MICROCMS_API_KEY,
+});
+
+//型定義
+export type Wooparoo = {
+  name: string;
+  image: Image;
+  egg_image: Image;
+  type: string[];
+  time: string[];
+};
+export type WooparooResponse = {
+  contents: Wooparoo[];
+}
+
+//APIの呼び出し
+export const getWooparoos = async (queries?: MicroCMSQueries) => {
+  return await client.get<WooparooResponse>({ endpoint: "wooparoos", queries });
+};
